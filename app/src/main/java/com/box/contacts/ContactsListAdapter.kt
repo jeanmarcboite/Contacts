7a17fa8212b564457ce8407cb76fd7ec67609431
box <jeanmarcboite@gmail.com>
2018-10-21 11:21:50 +0200

    contact -> contacts

--- app/src/main/java/com/box/contacts/ContactsListAdapter.kt
+++ app/src/main/java/com/box/contacts/ContactsListAdapter.kt
@@ -6,26 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class ContactListAdapter(private val myDataset: Array<String>) : RecyclerView.Adapter<ContactListAdapter.ContactListViewHolder>() {
+class ContactsListAdapter(private val myDataset: Array<String>) : RecyclerView.Adapter<ContactsListAdapter.ContactsListViewHolder>() {
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class ContactListViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class ContactsListViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
     // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): ContactListViewHolder {
+                                    viewType: Int): ContactsListViewHolder {
         // create a new view
         val contactView = LayoutInflater.from(parent.context)
             .inflate(R.layout.contact, parent, false)
 
         // set the view's size, margins, paddings and layout parameters
-        return ContactListViewHolder(contactView)
+        return ContactsListViewHolder(contactView)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: ContactListViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ContactsListViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.view.findViewById<TextView>(R.id.contactTextView).text = myDataset[position]
--- app/src/main/java/com/box/contacts/fragments/ContactsFragment.kt
+++ app/src/main/java/com/box/contacts/fragments/ContactsFragment.kt
@@ -5,12 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import com.box.contacts.ContactListAdapter
-import com.box.contacts.MainActivity
+import com.box.contacts.ContactsListAdapter
 import com.box.contacts.R
-import kotlinx.android.synthetic.main.contacts_list.view.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
 
 class ContactsFragment() : ViewPagerFragment() {
     override fun onCreateView(
@@ -19,7 +15,7 @@ class ContactsFragment() : ViewPagerFragment() {
     ): View? {
         val dataSet = arrayOf("ZERO", "one", "Two")
         val contactsList = inflater.inflate(R.layout.contacts_list, container, false) as RecyclerView
-        contactsList.adapter = ContactListAdapter(dataSet)
+        contactsList.adapter = ContactsListAdapter(dataSet)
         contactsList.layoutManager = LinearLayoutManager(this.context)
         return contactsList
     }
